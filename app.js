e2149595fed01ee4425389c73f561209d735eac4
Mateusz Burnagiel <mateuszburnagiel@gmail.com>
2021-09-28 19:03:14 +0200

    Finish app

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ const prepareDOMEvents = () => {
     addBtn.addEventListener('click', addNewTask);
     ulList.addEventListener('click', checkClick);
     popupCloseBtn.addEventListener('click', closePopup)
+    popupAddBtn.addEventListener('click', changeTaskText);
+    toDoInput.addEventListener('keyup', enterKeyCheck)
 }
 
 
@@ -36,7 +38,8 @@ const addNewTask = () => {
         toDoInput.value = '';
         errorInfo.textContent = '';
     } else {
-        errorInfo.textContent = "Wpisz treść zadania!"
+        errorInfo.textContent = "Wpisz treść zadania!";
+        errorInfo.style.color = "tomato";
     }
 }
 
@@ -60,28 +63,54 @@ const createToolsArea = () => {
     toolsPanel.append(complateBtn, editBtn, deleteBtn);
 }
 
-const checkClick = (e) => {
+const checkClick = e => {
     if (e.target.matches('.complete')) {
         e.target.closest('li').classList.toggle('completed');
         e.target.classList.toggle('completed')
 
     } else if (e.target.matches('.edit')) {
-       editToDo();
+        editTask(e);
 
     } else if (e.target.matches('.delete')) {
-        e.target.closest('li').classList.toggle('delete');
+        delateTask(e);
 
     } else {
         console.log('jakis blad');
     }
 }
 
-const editToDo = () => {
+const editTask = e => {
+    toDoEdit = e.target.closest('li');
+    popupInput.value = toDoEdit.firstChild.textContent;
     popup.style.display = 'flex';
 }
 
 const closePopup = () => {
     popup.style.display = 'none';
+}
+
+const changeTaskText = () => {
+    if (popupInput.value !== '') {
+        toDoEdit.firstChild.textContent = popupInput.value;
+    } else {
+        popupInfo.textContent = "Wpisz treść zadania!";
+    }
+}
+
+const delateTask = (e) => {
+    e.target.closest('li').remove();
+
+    const allTasks = ulList.querySelectorAll('li');
+
+    if (allTasks.length == 0) {
+        errorInfo.textContent = "Lista zadań jest pusta";
+    }
+}
+
+const enterKeyCheck = (e) => {
+    if (e.key === 'Enter') {
+        addNewTask();
+    }
 }
 
 document.addEventListener('DOMContentLoaded', main);
